910441fed37452f158831653b5ce1d4688ce1cf2
tanshihao <tanshihao@himobi.net>
2019-10-16 11:22:55 +0800

    提交任务模块model

---
+++ FindElectricity/FEWork/Model/FEWorkModel.m
@@ -0,0 +1,48 @@
+//
+//  FEWorkModel.m
+//  FindElectricity
+//
+//  Created by 豪锅锅 on 2019/10/13.
+//  Copyright © 2019 LiDongQiang. All rights reserved.
+//
+
+#import "FEWorkModel.h"
+
+@implementation FEWorkModel
+
+@end
+
+@implementation FEWorkPanelDataResponseModel
+
+
+@end
+
+@implementation FEWorkElectricityListResponseModel
+
+
+
+@end
+
+@implementation FEWorkShopInfoResponseModel
+
+
+
+@end
+
+@implementation FEWorkShopInfoResponseListModel
+
+
+
+@end
+
+@implementation FEWorkExhcangeGoodRequestModel
+
+
+
+@end
+
+@implementation FEWorkReceivegoodRequestModel
+
+
+
+@end
