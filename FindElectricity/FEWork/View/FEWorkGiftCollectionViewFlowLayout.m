6ec9c201e5283c32786e8bbd47a788bad061fe68
tanshihao <tanshihao@himobi.net>
2019-11-02 17:08:08 +0800

    commit 修复bug

--- FindElectricity/FEWork/View/FEWorkGiftCollectionViewFlowLayout.m
+++ FindElectricity/FEWork/View/FEWorkGiftCollectionViewFlowLayout.m
@@ -12,8 +12,15 @@
 @implementation FEWorkGiftCollectionViewFlowLayout
 - (id)init{
     if (self =[super init]) {
-        self.itemSize =CGSizeMake(WIDTH_LY(40), HEIGHT_LY(50));
-        self.sectionInset =UIEdgeInsetsMake(WIDTH_LY(20),  WIDTH_LY(10), 0,  WIDTH_LY(10));
+       
+    
+        if(SCREEN_HEIGHT>812||iPhone6Plus){
+            self.itemSize =CGSizeMake(WIDTH_LY(30), HEIGHT_LY(40));
+            self.sectionInset =UIEdgeInsetsMake(WIDTH_LY(10),  WIDTH_LY(10), 0,  WIDTH_LY(10));
+        }else {
+            self.itemSize =CGSizeMake(WIDTH_LY(40), HEIGHT_LY(50));
+             self.sectionInset =UIEdgeInsetsMake(WIDTH_LY(20),  WIDTH_LY(10), 0,  WIDTH_LY(10));
+        }
         self.minimumLineSpacing = WIDTH_LY(10);
         self.minimumInteritemSpacing = WIDTH_LY(10);
     }
